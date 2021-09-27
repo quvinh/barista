62dca68a38a4bc747d5a90f93f3ee97e78f5b137
quvinh <vinhhp2620@gmail.com>
2021-09-28 00:32:21 +0700

    edit frontend web, add icon, image

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
-const {Product} = require('../models/product')
+const { Product } = require('../models/product')
 // const {User} = require('../models/user')
-const {Product_type} = require('../models/product_type')
+const { Product_type } = require('../models/product_type')
 const User = require('../models/user')
 const passport = require('passport')
 
@@ -16,7 +16,7 @@ const url = process.env.MONGO_URI;
 
 //----------------------------
 router.get('/', async (req, res) => {
-    res.render('../views/page/home.ejs', {user: req.user})
+    res.render('../views/page/home.ejs', { user: req.user })
 })
 //----------------------------
 
@@ -25,7 +25,7 @@ router.get('/', async (req, res) => {
 //====================================================================
 
 router.get('/userprofile', isLoggedIn, (req, res) => {
-    res.render('../views/user/userprofile.ejs', {user: req.user})
+    res.render('../views/user/userprofile.ejs', { user: req.user })
 })
 
 router.get('/login', (req, res) => {
@@ -35,7 +35,7 @@ router.get('/login', (req, res) => {
 router.post('/login', passport.authenticate('local', {
     successRedirect: '/userprofile',
     failureRedirect: '/login'
-}),function(req, res){})
+}), function (req, res) { })
 
 router.get('/register', (req, res) => {
     res.render('../views/user/register.ejs')
@@ -45,12 +45,12 @@ router.post('/register', (req, res) => {
     User.register(new User({
         username: req.body.username,
         phone: req.body.phone
-    }), req.body.password, function(e, user){
-        if(e){
+    }), req.body.password, function (e, user) {
+        if (e) {
             console.log(e)
             res.render('../views/user/register.ejs')
         }
-        passport.authenticate('local')(req, res, function(){
+        passport.authenticate('local')(req, res, function () {
             res.redirect('/login')
         })
     })
@@ -61,8 +61,8 @@ router.get('/logout', (req, res) => {
     res.redirect('/')
 })
 
-function isLoggedIn(req, res, next){
-    if(req.isAuthenticated()){
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
         return next()
     }
     res.redirect('/login')
@@ -77,16 +77,22 @@ function isLoggedIn(req, res, next){
 //Get all product---------------------------------------------
 router.get('/product', isLoggedIn, async (req, res) => {
     const product = await Product.find({ user_id: req.user._id })
-    res.render('../views/product/index.ejs', {product: product})
+    res.render('../views/product/index.ejs', { product: product })
 })
 
 //Get simple product
 router.get('/product/:id', isLoggedIn, async (req, res) => {
     // const product = await Product.findById(req.params.id)
     // res.render('../views/product/simple_product.ejs', {product : product})
-    Product.findById(req.params.id, function(e, product){
-        Product_type.findById(product.product_type_id, function(e, product_type){
-            res.render('../views/product/simple_product.ejs', {product : product, product_type: product_type})
+    Product.findById(req.params.id, function (e, product) {
+        Product_type.findById(product.product_type_id, function (error, product_type) {
+            if(error) throw error;
+            if (product_type == null) {
+                // alert('Loại đồ uống ứng với topping đã bị xoá!\nBạn hãy chỉnh sửa "Đồ uống >> Sửa >> Tên loại đồ uống".Thx :3');
+                res.redirect('/');
+            } else {
+                res.render('../views/product/simple_product.ejs', { product: product, product_type: product_type })
+            }
         })
     })
 })
@@ -95,9 +101,9 @@ router.get('/product/:id', isLoggedIn, async (req, res) => {
 router.get('/product/edit/:id', isLoggedIn, async (req, res) => {
     // const product = await Product.findById(req.params.id)
     // res.render('../views/product/edit_product.ejs', {product : product})
-    Product.findById(req.params.id, function(e, product){
-        Product_type.find({user_id: req.user._id}, function(e, product_type){
-            res.render('../views/product/edit_product.ejs', {product : product, product_type: product_type})
+    Product.findById(req.params.id, function (e, product) {
+        Product_type.find({ user_id: req.user._id }, function (e, product_type) {
+            res.render('../views/product/edit_product.ejs', { product: product, product_type: product_type })
         })
     })
 })
@@ -134,13 +140,13 @@ router.put('/product/edit/:id', (req, res) => {
             tea_size_l: req.body.tea_size_l
         }
     }
-    Product.findByIdAndUpdate(req.params.id, {$set: product}, (e, data) => {
-        if(!e){
+    Product.findByIdAndUpdate(req.params.id, { $set: product }, (e, data) => {
+        if (!e) {
             // console.log(`send request`)
             // res.status(200).json({ code: 200, message: 'Product updated succesfully', updateProduct: data })
             res.redirect(`/product/${req.params.id}`)
-            
-        }else{
+
+        } else {
             console.log(e)
         }
     })
@@ -148,10 +154,10 @@ router.put('/product/edit/:id', (req, res) => {
 
 //New product
 router.get('/new/product', isLoggedIn, (req, res) => {
-    Product_type.find({user_id: req.user._id}, function(e, product_type) {
-        res.render('../views/product/new.ejs', {product: new Product(), product_type: product_type})
+    Product_type.find({ user_id: req.user._id }, function (e, product_type) {
+        res.render('../views/product/new.ejs', { product: new Product(), product_type: product_type })
     })
-    
+
 })
 // router.get('/new/product', async (req, res) => {
 //     const product_type = await Product_type.find()
@@ -204,13 +210,13 @@ router.post('/product', (req, res) => {
     // product.save()
     // res.redirect(`product/${req.params.id}`)
     return product
-    .save(function(e, newProduct) {
-        if(e){
-            return res.redirect(`new/product`)
-        }else{
-            return res.redirect('product/' + newProduct.id)
-        }
-    })
+        .save(function (e, newProduct) {
+            if (e) {
+                return res.redirect(`new/product`)
+            } else {
+                return res.redirect('product/' + newProduct.id)
+            }
+        })
     // .then(() => {
     //     return res.status(201).json({
     //         success: true,
@@ -239,20 +245,20 @@ router.delete('/product/del/:id', async (req, res) => {
 //====================================================================
 
 //Get all product_type
-router.get('/product_type', isLoggedIn, async(req, res) => {
-    const product_type = await Product_type.find({user_id: req.user._id})
-    res.render('../views/product_type/index.ejs', {product_type: product_type})
+router.get('/product_type', isLoggedIn, async (req, res) => {
+    const product_type = await Product_type.find({ user_id: req.user._id })
+    res.render('../views/product_type/index.ejs', { product_type: product_type })
 })
 
 // //Get product_type
-router.get('/product_type/:id', isLoggedIn, async(req, res) => {
+router.get('/product_type/:id', isLoggedIn, async (req, res) => {
     const product_type = await Product_type.findById(req.params.id)
     // if(product_type == null) res.redirect('/')
-    res.render('../views/product_type/simple_product_type.ejs', {product_type: product_type})
+    res.render('../views/product_type/simple_product_type.ejs', { product_type: product_type })
 })
 
 //Edit product_type
-router.get('/product_type/edit/:id', isLoggedIn, async(req, res) => {
+router.get('/product_type/edit/:id', isLoggedIn, async (req, res) => {
     const product_type = await Product_type.findById(req.params.id)
     res.render('../views/product_type/edit_product_type.ejs', { product_type: product_type })
 })
@@ -260,14 +266,14 @@ router.get('/product_type/edit/:id', isLoggedIn, async(req, res) => {
 //Update product_type
 router.put('/product_type/edit/:id', (req, res) => {
     const pro = {
-        name : req.body.name,
+        name: req.body.name,
         user_id: req.user._id
     }
-    Product_type.findByIdAndUpdate(req.params.id, {$set: pro}, (e, data) => {
-        if(!e){
+    Product_type.findByIdAndUpdate(req.params.id, { $set: pro }, (e, data) => {
+        if (!e) {
             // res.status(200).json({ code: 200, message: 'Product_type updated succesfully', updateProduct_type: data })
             res.redirect(`/product_type/${req.params.id}`)
-        }else{
+        } else {
             console.log(e)
         }
     })
@@ -285,11 +291,11 @@ router.post('/product_type', (req, res) => {
         user_id: req.user._id
     })
     product_type.save()
-    res.redirect(`/product_type/${ product_type.id }`)
+    res.redirect(`/product_type/${product_type.id}`)
 })
 
 //Delete product_type
-router.delete('/product_type/del/:id', async(req, res) => {
+router.delete('/product_type/del/:id', async (req, res) => {
     await Product_type.findByIdAndDelete(req.params.id)
     res.redirect('/product_type')
 })
@@ -297,26 +303,26 @@ router.delete('/product_type/del/:id', async(req, res) => {
 //====================================================================
 //                              TUTORIAL
 //====================================================================
-router.get('/tutorial', isLoggedIn, async(req, res) => {
+router.get('/tutorial', isLoggedIn, async (req, res) => {
     // const col = cb => {
     //     Product.listIndexes().toArray(cb);
     // }
     // console.log(col);
-    
+
     // const moreName = await Product.find({user_id: req.user._id});
     // res.render('../views/page/tutorial.ejs', { moreName: moreName, user: req.user });
-    Product.find({user_id: req.user._id}, function(e, moreName){
+    Product.find({ user_id: req.user._id }, function (e, moreName) {
         MongoClient.connect(url, function (err, db) {
             if (err) throw err;
             const dbo = db.db("db_barista");
             const ID = new String(req.user._id);
-            const query = {user_id: ID.valueOf()};
+            const query = { user_id: ID.valueOf() };
             console.log(query);
             dbo.collection("products").find(query).toArray(function (err, result) {
                 if (err) throw err;
-                Product.find({user_id: req.user._id}).countDocuments().then((countDoc) => {
+                Product.find({ user_id: req.user._id }).countDocuments().then((countDoc) => {
                     console.log(result);
-                    res.render('../views/page/tutorial.ejs', { moreName: moreName, result: result, countDoc: countDoc});
+                    res.render('../views/page/tutorial.ejs', { moreName: moreName, result: result, countDoc: countDoc });
                 })
                 db.close();
             })
